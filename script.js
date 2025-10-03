0ccd132db77915b2f3569eaf71cfd0b447158e6d
Nonnani-cs <daungsakulnutthanon@gmail.com>
2025-10-03 22:42:38 +0700

    non
    finish

---
+++ script.js
@@ -0,0 +1,27 @@
+(function($) {
+  "use strict"; 
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 70
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+  var navbarCollapse = function() {
+    if ($("#mainNav").length > 0 && $("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  navbarCollapse();
+  $(window).scroll(navbarCollapse);
+})(jQuery);
